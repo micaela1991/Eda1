8f7c64c1129303e40408a6053fa44a45fcdf6341
micaela1991 <133174243+micaela1991@users.noreply.github.com>
2024-09-09 14:42:30 -0300

    Ejercicio 1.c

--- ejercicio 1.c
+++ ejercicio 1.c
@@ -1,22 +1,24 @@
+/*1- Escriba un main en el que se declare un vector de números enteros, entre 10 y 15 valores, y que
+muestre dichos valores por pantalla usando un puntero para hacerlo.*/
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-
-int main(void){
-
-short vector[]={ 20,30,4,5,6,27,8,9,11,12};
-short cantidad;
-short *Puntero_vector=NULL;
-short indice;
-Puntero_vector=vector; 
-cantidad=sizeof(vector)/sizeof(short);
-
-for (indice=0;indice<cantidad;indice++)
+int main(void)
 {
-    printf("\nvector[%hd]: %hd",indice,*Puntero_vector ++);
-}
 
+    short vector[] = {20, 30, 4, 5, 6, 27, 8, 9, 11, 12,6,9};
+    short cantidad;
+    short *Puntero_vector = NULL;
+    short indice;
+    
+    Puntero_vector = vector;
+    
+    cantidad = sizeof(vector) / sizeof(short);
 
-return 0;
+    for (indice = 0; indice < cantidad; indice++)
+    {
+        printf("\nvector[%hd]: %hd", indice, *(Puntero_vector+indice));
+    }
+
+    return 0;
 }
